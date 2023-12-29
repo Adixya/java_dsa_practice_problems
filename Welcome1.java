9c4a5364fae39cb362558a72979a5951952e75bf
Aditya Singh <114297309+Adixya@users.noreply.github.com>
2023-12-29 22:40:40 +0530

    Add files via upload

---
+++ Welcome1.java
@@ -0,0 +1,6 @@
+class Welcome1 {
+    public static void main (String []args){
+        System.out.println("Welcome to java programming!");
+        System.out.println("*\n**\n***\n****\n*****");
+    }
+}
---
+++ variables.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class variables {
+    // Datatypes
+    public static void main(String[] args){
+        // Variables
+        // int a = 10;
+        // int b = 5;
+        // int mul = a * b;
+        // int diff = a - b;
+        // int div = mul/diff;
+        // System.out.println(div);
+        // int ans = (a * b) / (a - b);
+        // System.out.println(ans);
+        // Input & Variables
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
