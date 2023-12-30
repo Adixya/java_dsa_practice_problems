3ae2aeec2e480b7ebd42c64241c75c8b279e458b
Aditya Singh <114297309+Adixya@users.noreply.github.com>
2023-12-30 19:54:14 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+public class Loops {
+    public static void main(String args[]){
+        // FOR LOOP
+        // for(int i = 0; i < 11; i = i + 1){
+        //     System.out.print(i+" ");
+        // }
+
+        // WHILE LOOP
+        // int i = 0;
+        // while(i<11){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // DO WHILE LOOP
+        // int i = 0;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // } while(i < 11);
+
+        // Sum of N numbers
+        // Scanner scan = new Scanner(System.in);
+        // int n = scan.nextInt();
+        // int sum = 0;
+        // for(int i = 1; i <= n; i++){
+        //     sum = sum + i;
+        // }
+        // System.out.println(sum);
+        
+        //TABLE FOR n
+        // Scanner scan = new Scanner(System.in);
+        // int n = scan.nextInt();
+        // for(int i = 1; i<11; i++){
+        //     System.out.println(n+" * "+i+" = "+(n*i) );
+        // }
+        
+        //EVEN no till n
+        // Scanner scan = new Scanner(System.in);
+        // int n = scan.nextInt();
+        // for(int i = 2; i <= n; i = i + 2){
+        //     if(i % 2 ==0){
+        //         System.out.println(i);
+        //     } 
+        // }
+
+        // Make a menu driven program. The user can enter 2 numbers, either 1 or 0. 
+        // System.out.print("Enter 0 or 1 : ");
+        // Scanner sc = new Scanner(System.in);
+        // int menu = sc.nextInt();
+        // int n=0;
+        // switch(menu){
+        //     case 1 :if(menu == 1){
+        //                 do{ 
+        //                     int marks = sc.nextInt();
+        //                     if(marks >= 90){
+        //                         System.out.println("Good");
+        //                     } else if(marks <= 89 && marks >= 60){
+        //                         System.out.println("also Good");
+        //                     } else if(marks <= 59 && marks>=0){
+        //                         System.out.println("Good as well");
+        //                     }
+        //                     n = sc.nextInt();
+        //                     if(n==0){
+        //                         break;
+        //                     }
+        //                 }while(n==1);
+        //             }
+        //     case 2 :if(menu == 0){
+        //         System.out.println("STOP");
+        //         break;
+        //     }
+        //     case 3 :if(menu > 1 || menu < 0){
+        //         System.out.println("Invalid input");
+        //         break;
+        //     }
+        
+        // }
+        
+    }
+}
